7a1349f6a37bca3d423a509b68cff1bf062f421e
Anastasios Chondrogiannis <info@anastasioscho.gr>
2018-07-20 13:03:37 +0300

    refactor: Use a functional component for the bookshelf

--- src/BookshelfComponent.js
+++ src/BookshelfComponent.js
@@ -1,31 +1,29 @@
-import React, {Component} from 'react'
+import React from 'react'
 import BookComponent from './BookComponent'
 import PropTypes from 'prop-types'
 
-class BookshelfComponent extends Component {
-    render() {
-        const {shelfTitle, shelfBooks, onShelfChange} = this.props
+function BookshelfComponent(props) {
+    const {shelfTitle, shelfBooks, onShelfChange} = props
 
-        return (
-            <div className="bookshelf">
-                <h2 className="bookshelf-title">{shelfTitle}</h2>
-                <div className="bookshelf-books">
-                    <ol className="books-grid">
-                    {shelfBooks.map(shelfBook => {
-                        return (
-                            <li key={shelfBook.id}>
-                                <BookComponent
-                                    book={shelfBook}
-                                    onShelfChange={onShelfChange}
-                                />
-                            </li>
-                        )
-                    })}
-                    </ol>
-                </div>
+    return (
+        <div className="bookshelf">
+            <h2 className="bookshelf-title">{shelfTitle}</h2>
+            <div className="bookshelf-books">
+                <ol className="books-grid">
+                {shelfBooks.map(shelfBook => {
+                    return (
+                        <li key={shelfBook.id}>
+                            <BookComponent
+                                book={shelfBook}
+                                onShelfChange={onShelfChange}
+                            />
+                        </li>
+                    )
+                })}
+                </ol>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 BookshelfComponent.propTypes = {
